656e64df46dc689c7c2e8eaa91f8f342a5bf9b7c
Anjali Suman <97871843+AnjaliSuman3011@users.noreply.github.com>
2025-07-31 16:12:09 +0530

    Update cards.ts
    Added placemnt stats

--- src/data/cards.ts
+++ src/data/cards.ts
@@ -19,7 +19,7 @@ export const gridSections: GridSection[] = [
         title: 'Quarterly Placement Report',
         description: 'View the Total Opportunities & CTC achieved by Crio.Do learners.',
         imageUrl: 'https://directus.crio.do/assets/a3f12389-511f-4af7-a978-e6d8dcfdec22.webp',
-        linkUrl: 'https://bit.ly/3EledF2',
+        linkUrl: 'https://drive.google.com/file/d/1RVUDhaRJxaCQbnlBdOnMSaUD2oYb3816/view?usp=sharing',
         buttonText: 'Click Here',
         identifier: 'Placement Stats Dashboard',
       },
